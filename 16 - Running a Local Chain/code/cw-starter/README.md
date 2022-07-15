7e98e47e33ed1b3ac322a84586356d24257d088f
Callum Anderson <callumanderson745@gmail.com>
2022-07-15 23:27:22 +0100

    Remove super out of date stuff

--- 16 - Running a Local Chain/code/cw-starter/README.md
+++
@@ -1,1 +0,0 @@
-# Part 16 Code
--- 17 - Front End Template/code/cw-starter-front/util/fee.ts
+++
@@ -1,6 +0,0 @@
-import { coins, StdFee, Coin } from "@cosmjs/stargate";
-
-export const defaultExecuteFee: StdFee = {
-  amount: coins(5000, process.env.NEXT_PUBLIC_STAKING_DENOM!),
-  gas: "1000000",
-};
